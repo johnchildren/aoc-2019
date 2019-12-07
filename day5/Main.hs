e20ebf32580cd76095ca0a1b663bddefdd272bdc
John Children <john.a.children@gmail.com>
2019-12-07 09:53:08 +0000

    Days 5 and 6

---
+++ day5/Main.hs
@@ -0,0 +1,165 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ViewPatterns #-}
+
+module Main
+  ( main
+  )
+where
+
+import           Data.Text                                ( Text )
+import qualified Data.Text                     as Text
+import qualified Data.Text.IO                  as Text
+import           Data.Vector                              ( (!?)
+                                                          , (//)
+                                                          , Vector
+                                                          )
+import qualified Data.Vector                   as Vector
+
+type Program = Vector Int
+type Pointer = Int
+type Offset = Int
+type Input = Vector Int
+type Output = Vector Int
+
+data Mode = Position | Immediate
+
+data Instr where
+  Add :: (Int, Mode) -> (Int, Mode) -> (Int, Mode) -> Instr
+  Mul :: (Int, Mode) -> (Int, Mode) -> (Int, Mode) -> Instr
+  Input :: (Int, Mode) -> Instr
+  Output :: (Int, Mode) -> Instr
+  JumpIfTrue :: (Int, Mode) -> (Int, Mode) -> Instr
+  JumpIfFalse :: (Int, Mode) -> (Int, Mode) -> Instr
+  LessThan :: (Int, Mode) -> (Int, Mode) -> (Int, Mode) -> Instr
+  Equals :: (Int, Mode) -> (Int, Mode) -> (Int, Mode) -> Instr
+  Terminate :: Instr
+
+run :: Program -> Input -> (Output, Program)
+run initialProg input =
+  let (_, o, finalProg) = go 0 (input, Vector.empty, initialProg)
+  in  (o, finalProg)
+ where
+  go :: Pointer -> (Input, Output, Program) -> (Input, Output, Program)
+  go _ (i, o, prog@(Vector.null -> True)          ) = (i, o, prog)
+  go p (i, o, prog@(readInstr p -> (_, Terminate))) = (i, o, prog)
+  go p (i, o, prog@(readInstr p -> (offset, Add arg1 arg2 (instrOut, Position))))
+    = go (p + offset)
+         (i, o, prog // [(instrOut, evalArg prog arg1 + evalArg prog arg2)])
+  go p (i, o, prog@(readInstr p -> (offset, Mul arg1 arg2 (instrOut, Position))))
+    = go (p + offset)
+         (i, o, prog // [(instrOut, evalArg prog arg1 * evalArg prog arg2)])
+  go p (i, o, prog@(readInstr p -> (offset, Input (instrOut, Position)))) = go
+    (p + offset)
+    (Vector.tail i, o, prog // [(instrOut, Vector.head input)])
+  go p (i, o, prog@(readInstr p -> (offset, Output arg1))) =
+    go (p + offset) (i, o `Vector.snoc` evalArg prog arg1, prog)
+  go p (i, o, prog@(readInstr p -> (offset, JumpIfTrue arg1 arg2))) = go
+    (if evalArg prog arg1 /= 0 then evalArg prog arg2 else p + offset)
+    (i, o, prog)
+  go p (i, o, prog@(readInstr p -> (offset, JumpIfFalse arg1 arg2))) = go
+    (if evalArg prog arg1 == 0 then evalArg prog arg2 else p + offset)
+    (i, o, prog)
+  go p (i, o, prog@(readInstr p -> (offset, LessThan arg1 arg2 (instrOut, Position))))
+    = go
+      (p + offset)
+      ( i
+      , o
+      , prog
+        // [(instrOut, if evalArg prog arg1 < evalArg prog arg2 then 1 else 0)]
+      )
+  go p (i, o, prog@(readInstr p -> (offset, Equals arg1 arg2 (instrOut, Position))))
+    = go
+      (p + offset)
+      ( i
+      , o
+      , prog
+        // [(instrOut, if evalArg prog arg1 == evalArg prog arg2 then 1 else 0)]
+      )
+  go _ (i, o, prog) = (i, o, prog)
+
+evalArg :: Program -> (Int, Mode) -> Int
+evalArg prog (p, Position) = case prog !? p of
+  Just arg -> arg
+  Nothing  -> error $ "no entry at position: " ++ show p
+evalArg _ (n, Immediate) = n
+
+readArg :: Program -> Pointer -> Offset -> Int -> (Int, Mode)
+readArg prog p offset codes =
+  let code = case (codes `quot` (10 ^ (offset - 1))) `rem` 10 of
+        0 -> Position
+        1 -> Immediate
+        n -> error ("unknown mode code: " ++ show n)
+  in  case prog !? (p + offset) of
+        Just newP -> (newP, code)
+        Nothing ->
+          error
+            $  "no arg at position: "
+            ++ show (p + offset)
+            ++ " full prog: "
+            ++ show prog
+
+readInstr :: Pointer -> Program -> (Offset, Instr)
+readInstr p prog
+  = let opcode = case prog !? p of
+          Just fullcode -> fullcode
+          Nothing       -> error $ "no code at pointer position: " ++ show p
+    in
+      case opcode `quotRem` 100 of
+        (codes, 1) ->
+          ( 4
+          , Add (readArg prog p 1 codes)
+                (readArg prog p 2 codes)
+                (readArg prog p 3 codes)
+          )
+        (codes, 2) ->
+          ( 4
+          , Mul (readArg prog p 1 codes)
+                (readArg prog p 2 codes)
+                (readArg prog p 3 codes)
+          )
+        (codes, 3) -> (2, Input (readArg prog p 1 codes))
+        (codes, 4) -> (2, Output (readArg prog p 1 codes))
+        (codes, 5) ->
+          (3, JumpIfTrue (readArg prog p 1 codes) (readArg prog p 2 codes))
+        (codes, 6) ->
+          (3, JumpIfFalse (readArg prog p 1 codes) (readArg prog p 2 codes))
+        (codes, 7) ->
+          ( 4
+          , LessThan (readArg prog p 1 codes)
+                     (readArg prog p 2 codes)
+                     (readArg prog p 3 codes)
+          )
+        (codes, 8) ->
+          ( 4
+          , Equals (readArg prog p 1 codes)
+                   (readArg prog p 2 codes)
+                   (readArg prog p 3 codes)
+          )
+        (_    , 99) -> (0, Terminate)
+        (codes, n ) -> error
+          (  "unknown op code: '"
+          ++ show n
+          ++ "' with positional codes: '"
+          ++ show codes
+          ++ "' at position: '"
+          ++ show p
+          ++ "' and program: "
+          ++ show prog
+          )
+
+parse :: Text -> Vector Int
+parse s = Vector.fromList $ read @Int . Text.unpack <$> Text.splitOn "," s
+
+main :: IO ()
+main = do
+  input <- Text.readFile "day5/input.txt"
+  let program = parse input
+
+  putStr "part1: "
+  print $ fst $ run program (Vector.singleton 1)
+
+  putStr "part2: "
+  print $ fst $ run program (Vector.singleton 5)
---
+++ day6/Main.hs
@@ -0,0 +1,76 @@
+module Main
+  ( main
+  )
+where
+
+import           Data.Either                              ( either
+                                                          , lefts
+                                                          , rights
+                                                          )
+import           Data.Maybe                               ( catMaybes )
+import           Data.Tree
+import           Data.Text                                ( Text )
+import qualified Data.Text                     as Text
+import qualified Data.Text.IO                  as Text
+
+type Orbits = [(Text, Text)]
+type OrbitTree = Tree Text
+
+-- Catamorphism which keeps track of the number of child nodes seen
+totalOrbits :: OrbitTree -> Int
+totalOrbits = fst . foldTree
+  (\_ xs ->
+    let totalChildren = length xs + sum (snd <$> xs)
+    in  (totalChildren + sum (fst <$> xs), totalChildren)
+  )
+
+type Transfer = Either (Maybe Int, Maybe Int) Int
+
+-- fold the tree and count with some precedence rules
+findTransfer :: Text -> Text -> OrbitTree -> Maybe Int
+findTransfer start end tree = either (const Nothing) (Just)
+  $ foldTree findTransfer' tree
+ where
+  findTransfer' :: Text -> [Transfer] -> Transfer
+  findTransfer' l ts | l == start =
+    case (catMaybes $ fst <$> lefts ts, catMaybes $ snd <$> lefts ts) of
+      (_ , []   ) -> Left (Just 0, Nothing)
+      ([], n : _) -> Right (n + 1)
+  findTransfer' l ts | l == end =
+    case (catMaybes $ fst <$> lefts ts, catMaybes $ snd <$> lefts ts) of
+      ([]   , _ ) -> Left (Nothing, Just 0)
+      (n : _, []) -> Right (n + 1)
+  findTransfer' _ ts =
+    case
+        (catMaybes $ fst <$> lefts ts, catMaybes $ snd <$> lefts ts, rights ts)
+      of
+        ([]    , []    , []   ) -> Left (Nothing, Nothing)
+        (n : _ , []    , _    ) -> Left (Just (n + 1), Nothing)
+        ([]    , n : _ , _    ) -> Left (Nothing, Just (n + 1))
+        (n1 : _, n2 : _, _    ) -> Right (n1 + n2)
+        (_     , _     , n : _) -> Right n
+
+buildOrbitTree :: Orbits -> OrbitTree
+buildOrbitTree orbits = unfoldTree
+  (\label -> (label, snd <$> filter (\(l, _) -> l == label) orbits))
+  "COM"
+
+parse :: Text -> Orbits
+parse s =
+  (\xs -> (head xs, head $ tail xs)) . Text.splitOn ")" <$> Text.lines s
+
+main :: IO ()
+main = do
+  input <- Text.readFile "day6/input.txt"
+
+  let orbits    = parse input
+  let orbitTree = buildOrbitTree orbits
+
+  --putStrLn $ drawTree (Text.unpack <$> orbitTree)
+
+  putStr "part1: "
+  print $ totalOrbits orbitTree
+
+  let transfer = findTransfer "YOU" "SAN" orbitTree
+  putStr "part2: "
+  print $ transfer
